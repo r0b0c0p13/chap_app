e575e9796964dd784f40b7feedde0f40e59aaacb
r0b0c0p13 <rdodrai@gmail.com>
2019-05-31 15:22:36 +0530

    init commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const express = require("express");
+var app = express();
+const publicpath = path.join(__dirname,"../public");
+const port = process.env.PORT || 3000;
+app.use(express.static(publicpath));
+
+app.listen(port,()=>{
+  console.log("server is started");
+});
